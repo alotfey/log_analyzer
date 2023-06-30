4549aefab571ac4c6186e9293afc63aa6d5a29e7
lotfey <lotfey@MacBook-Pro.local>
2023-06-29 23:23:40 -0400

    add function to search for login events alert

--- alert_events.py
+++ alert_events.py
@@ -5,7 +5,7 @@ class AlertEvents:
     def __init__(self):
         self.alert_events = []
 
-    def add_alert_event(self, alert_events):
+    def alert_login(self, alert_events):
         """Add alert event to list."""
         for alert_event in alert_events:
             if "login" in alert_event:
@@ -13,5 +13,5 @@ class AlertEvents:
         return self.alert_events
 
 log_events = event_handler.EventHandler("apache.log").get_request_url()
-alert_events = AlertEvents().add_alert_event(log_events)
+alert_events = AlertEvents().alert_login(log_events)
 print(alert_events)
