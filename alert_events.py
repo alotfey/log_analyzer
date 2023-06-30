77177b314ee332ad248dce2326be5a8c11150f94
lotfey <lotfey@MacBook-Pro.local>
2023-06-29 23:42:26 -0400

    Add alert ip function for alert ips

--- alert_events.py
+++ alert_events.py
@@ -3,7 +3,9 @@ import event_handler
 
 class AlertEvents:
     def __init__(self):
-        self.alert_events = []
+        self.alert_login_events = []
+        self.alert_ip_events = []
+        self.alert_ip_list = ["83.149.9.216","208.115.111.72"]
 
     def alert_login(self, alert_events):
         """Add alert event to list."""
@@ -11,7 +13,12 @@ class AlertEvents:
             if "login" in alert_event:
                 self.alert_events.append(alert_event)
         return self.alert_events
+    def alert_ips(self, alert_events):
+        """Add alert event to list."""
+        for ip in self.alert_ip_list:
+            if ip in alert_events:
+                self.alert_ip_events.append(ip)
+        return self.alert_ip_events
 
-log_events = event_handler.EventHandler("apache.log").get_request_url()
-alert_events = AlertEvents().alert_login(log_events)
-print(alert_events)
+log_events = event_handler.EventHandler("apache.log").collect_ips()
+alert_events = AlertEvents().alert_ips(log_events)
